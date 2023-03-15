617805263421136faf9f8cd0ee37c8681968ece5
Build Er <builder@macmini6.local>
2023-03-15 10:44:24 -0300

    Package version: 1.0.0-beta+20230315133647

--- Package.swift
+++ Package.swift
@@ -17,14 +17,14 @@ let package = Package(
 		.target(name: "GXCoreModule_SD_StoreWrapper",
 				dependencies: [
 					"GXCoreModule_SD_Store",
-					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.tvOS, .iOS])),
-					.product(name: "GXOpenSSL", package: "openssl-apple", condition: .when(platforms: [.tvOS, .iOS]))
+					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.iOS, .tvOS])),
+					.product(name: "GXOpenSSL", package: "openssl-apple", condition: .when(platforms: [.iOS, .tvOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_Store",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Store-1.0.0-beta+20230315120127.xcframework.zip",
-			checksum: "ba1e3a8ca8749323208f6f51f7beedd4a402f323216ff7eb820f5b0fba35369b"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Store-1.0.0-beta+20230315133647.xcframework.zip",
+			checksum: "49256aec8fb00cb4c5dc6897608be671659ad1ea8535fc6f020dae70befac4df"
 		)
 	]
 )
