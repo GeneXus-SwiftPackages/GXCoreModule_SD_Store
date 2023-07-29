2cfadd566bb4a16148aa740c793aa43d577cbc0d
genexusbot <builder@genexus.com>
2023-07-29 18:47:06 -0300

    Package version: 1.1.0-rc.1

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXCoreModule_SD_Store",
+	platforms: [.iOS("12.0"), .tvOS("12.0")],
+	products: [
+		.library(
+			name: "GXCoreModule_SD_Store",
+			targets: ["GXCoreModule_SD_StoreWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "1.1.0-rc.1"),
+		.package(url: "https://github.com/genexuslabs/openssl-apple", .upToNextMinor(from: "1.1.101"))
+	],
+	targets: [
+		.target(name: "GXCoreModule_SD_StoreWrapper",
+				dependencies: [
+					"GXCoreModule_SD_Store",
+					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.tvOS, .iOS])),
+					.product(name: "GXOpenSSL", package: "openssl-apple", condition: .when(platforms: [.tvOS, .iOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXCoreModule_SD_Store",
+			url: "https://pkgs.genexus.dev/iOS/releases/GXCoreModule_SD_Store-1.1.0-rc.1.xcframework.zip",
+			checksum: "523a5f4fbb8fedc523294b1f13021669ac2416d62187f5c9732077cef3d9d06c"
+		)
+	]
+)
